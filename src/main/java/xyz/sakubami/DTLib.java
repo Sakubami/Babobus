a0152f028c062eb7ecd62fba4cd04d3747472f9c
BlackHorizon176 <73308598+Sakubami@users.noreply.github.com>
2024-07-18 21:45:38 +0200

    test commit

--- src/main/java/xyz/sakubami/DTLib.java
+++ src/main/java/xyz/sakubami/DTLib.java
@@ -4,44 +4,16 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import xyz.sakubami.framework.logic.SilverManager;
 import xyz.sakubami.framework.utils.YMLHandler;
-import xyz.sakubami.listener.BlockPlaceListener;
 
 import java.io.IOException;
 import java.util.Random;
 import java.util.logging.Logger;
 
-public final class DTLib extends JavaPlugin {
+public final class DTLib {
 
-    Logger logger;
     public static Random randomGenerator;
-    public static YMLHandler groupData;
-    public static DTLib instance;
 
-    @Override
-    public void onEnable() {
+    public static void initLibrary() {
         randomGenerator = new Random();
-        SilverManager.init();
-        instance = this;
-        logger = getLogger();
-
-        getConfig().options().copyDefaults();
-        saveDefaultConfig();
-
-
-        listenerRegistry();
-        try {
-            groupData = new YMLHandler("groups.yml", this.getDataFolder());
-        } catch (IOException e) {
-            System.out.print(e);
-        }
-    }
-
-    public void listenerRegistry() {
-        Bukkit.getPluginManager().registerEvents(new BlockPlaceListener(this), this);
-    }
-
-    @Override
-    public void onDisable() {
-        groupData.unloadYAML();
     }
 }
